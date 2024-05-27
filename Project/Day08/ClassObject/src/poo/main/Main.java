69b08ac92134536d5df56fdd50094a7a92a564d8
laiscrz <lais.albino2003@gmail.com>
2024-05-27 16:46:00 -0300

    feat: add project 'ClassObject' done

--- Project/Day08/ClassObject/src/poo/main/Main.java
+++ Project/Day08/ClassObject/src/poo/main/Main.java
@@ -1,5 +1,19 @@
 package poo.main;
 
+import poo.model.ClassePessoa;
+
 public class Main {
 
+    public static void main(String[] args) {
+        
+       // criar obj
+       ClassePessoa pes = new ClassePessoa();
+       
+       // add valores aos atributos
+       pes.name = "Maria";
+       pes.idade = 25;
+       
+       // chamando metodos
+       pes.apresentar();
+    }
 }
--- Project/Day08/ClassObject/src/poo/model/ClassePessoa.java
+++ Project/Day08/ClassObject/src/poo/model/ClassePessoa.java
@@ -2,4 +2,20 @@ package poo.model;
 
 public class ClassePessoa {
 
+    // atributes
+	// acesso do atributo => public ou private
+	// tipo do atributo => int, String
+	// nome do atributo
+	
+    public String name;
+    public int idade;
+
+    // metodo da class
+    // acesso => public
+    // tipo => void (não retorna nada)
+    // nome => apresentar
+    public void apresentar(){
+        System.out.println("Olá, meu nome é " + name);
+        System.out.println("Tenho " + idade + " anos.");
+    }
 }
