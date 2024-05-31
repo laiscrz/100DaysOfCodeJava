c2eda5e3675c2fe4d33d6f82c8b0858a4a2eaf9b
laiscrz <lais.albino2003@gmail.com>
2024-05-31 15:27:37 -0300

    feat: add project struture

---
+++ Project/Day09/AnimalsProjectHierarchy/src/poo/model/Animal.java
@@ -0,0 +1,13 @@
+package poo.model;
+
+public class Animal {
+
+	// atributos
+	private String nome;
+	private int idade;
+	private double peso;
+	private int quantidadePatas;
+
+	// get e sets
+	
+}
